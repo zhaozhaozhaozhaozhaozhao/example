fb7536f127ce276db3982656c181dd7f4a3206d0
demo1 <1831424039@qq.com>
2024-05-06 10:39:42 +0800

    first commit

---
+++ src/main/java/com/demo22/example/zhao/gitTest.java
@@ -0,0 +1,7 @@
+package com.demo22.example.zhao;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
---
+++ src/test/java/com/demo22/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo22.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
