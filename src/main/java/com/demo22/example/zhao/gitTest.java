9a7cfce238e4af0e5e047407fe484716e8cca2db
demo1 <1831424039@qq.com>
2024-05-06 15:09:51 +0800

    push-test

--- src/main/java/com/demo22/example/zhao/gitTest.java
+++ src/main/java/com/demo22/example/zhao/gitTest.java
@@ -5,5 +5,6 @@ public class gitTest {
         System.out.println("hello Git");
         System.out.println("hello Git2");
         System.out.println("hello Git3");
+        System.out.println("hello Git4");
     }
 }
