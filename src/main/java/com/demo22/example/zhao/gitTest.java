1ebf9f5967f8b4c1e296f8bc6c13688cd8685cdc
demo1 <1831424039@qq.com>
2024-05-06 10:46:17 +0800

    second commit

--- src/main/java/com/demo22/example/zhao/gitTest.java
+++ src/main/java/com/demo22/example/zhao/gitTest.java
@@ -3,5 +3,6 @@ package com.demo22.example.zhao;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello Git");
+        System.out.println("hello Git2");
     }
 }
